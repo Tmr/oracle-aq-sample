efe01b58b67c623281abac4d55bd0047c29efff6
Tmr <timur.br@gmail.com>
2019-11-18 17:51:22 +0300

    Add simple message passing. It needs some jars from Oracle installation, I have no Oracle RDBMS, so can't check works this or not.

--- src/main/java/com/mkb/aqtransfer/OracleAQClient.java
+++ src/main/java/com/mkb/aqtransfer/OracleAQClient.java
@@ -19,7 +19,7 @@ public class OracleAQClient {
     public static void main(String[] args) {
         try {
             ClientConfig config = ClientConfig.getConfig();
-            if (args.length > 1 && args[1].equals("--all")) {
+            if (args.length > 0 && args[0].equals("--all")) {
                 transferAll(config);
             } else
                 transferMessage(config);
